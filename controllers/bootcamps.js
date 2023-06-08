cc2a66b2330b1991175f538fa7208bab40ceed1d
isadora lopes <isadora.c.lopes@ibm.com>
2023-06-08 19:19:04 -0300

    Setting up routes, controllers and middleware

---
+++ controllers/bootcamps.js
@@ -0,0 +1,34 @@
+// @desc     Get all bootcamps
+// @route    GET /api/v1/bootcamps
+// @access   Public
+exports.getBootcamps = (req, res, next) => {
+    res.send('Hello');
+}
+
+// @desc     Get single bootcamp
+// @route    GET /api/v1/bootcamps/:id
+// @access   Public
+exports.getBootcamp = (req, res, next) => {
+    res.send('Hello');
+}
+
+// @desc     Create new bootcamp
+// @route    POST /api/v1/bootcamps
+// @access   Private
+exports.createBootcamp = (req, res, next) => {
+    res.send('Hello');
+}
+
+// @desc     Update bootcamp
+// @route    PUT /api/v1/bootcamps
+// @access   Private
+exports.updateBootcamp = (req, res, next) => {
+    res.send('Hello');
+}
+
+// @desc     Delete bootcamp
+// @route    DELETE /api/v1/bootcamps/:id
+// @access   Private
+exports.deleteBootcamp = (req, res, next) => {
+    res.send('Hello');
+}
