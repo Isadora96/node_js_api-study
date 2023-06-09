268efa596a3d1ad9b722f0e45fabe5e6fe83c5be
isadora lopes <isadora.c.lopes@ibm.com>
2023-06-09 14:33:29 -0300

    Connecting mongoose and updating crud api

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
 
+connectDB();
+
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+app.use(express.json());
 
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -16,5 +22,11 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
+process.on('unhandleRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+
+    server.close(() => process.exit(1))
+})
 
