9df9264284a6c3d18a3a46ca1c40680c62f6d7e0
isadora lopes <isadora.c.lopes@ibm.com>
2023-06-15 17:07:23 -0300

    seeder and radius

---
+++ seeder.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// Load env vars
+dotenv.config({ path: './config/config.env'});
+
+// Load models
+const Bootcamp = require('./models/Bootcamp');
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: false
+});
+
+const bootcamps = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+const importData = async () => {
+    try {
+        await Bootcamp.create(bootcamps);
+
+        console.log('Data Imported...'.green.inverse)
+        process.exit();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const deleteData = async () => {
+    try {
+        await Bootcamp.deleteMany();
+
+        console.log('Data Destroyed...'.red.inverse)
+        process.exit();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//process.argv [0] node [1] seeder [2] -i
+if(process.argv[2] === '-i') {
+    importData();
+} else if(process.argv[2] === '-d') {
+    deleteData();
+}
