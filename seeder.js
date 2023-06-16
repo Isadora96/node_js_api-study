f9f43664450fa99081af78007fdb5fcdc8cf107b
isadora lopes <isadora.c.lopes@ibm.com>
2023-06-16 17:30:37 -0300

    Course model & seeder

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env'});
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -18,10 +19,14 @@ mongoose.connect(process.env.MONGO_URI, {
 const bootcamps = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
+const course = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
 
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
+        await Course.create(course);
 
         console.log('Data Imported...'.green.inverse)
         process.exit();
@@ -33,6 +38,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
+        await Course.deleteMany();
 
         console.log('Data Destroyed...'.red.inverse)
         process.exit();
