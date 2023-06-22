713224b2428f776ee514fc8db5747cdb5d42d20b
isadora lopes <isadora.c.lopes@ibm.com>
2023-06-22 15:41:21 -0300

    User login

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
 
 // @desc    Register user
-// @route   GET /api/v1/auth/register
+// @route   POST /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
     const { name, email, password, role } = req.body;
@@ -14,6 +14,33 @@ exports.register = asyncHandler(async (req, res, next) => {
         password,
         role
     });
+
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({ success: true, token });
+});
+
+// @desc    Register user
+// @route   POST /api/v1/auth/login
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if(!email || !password) {
+        return next(new ErrorResponse('Please provide an email and password'), 400);
+    }
+
+    const user = await User.findOne({email}).select('+password');
+    
+    if(!user) {
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+
+    const isMatch = await user.matchPassword(password);
+
+    if(!isMatch) {
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
 
     const token = user.getSignedJwtToken();
 
