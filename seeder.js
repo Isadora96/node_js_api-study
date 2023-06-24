d01f077985926adec8a311fcb097fb770a7d1c8d
isadora lopes <isadora.c.lopes@ibm.com>
2023-06-24 18:15:18 -0300

    Bootcamp & User Relationship

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: './config/config.env'});
 // Load models
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -22,11 +23,15 @@ const bootcamps = JSON.parse(
 const course = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 );
+const user = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
 
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
         await Course.create(course);
+        await User.create(user);
 
         console.log('Data Imported...'.green.inverse)
         process.exit();
@@ -38,7 +43,8 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
-        await Course.deleteMany();
+        await Course.deleteMany();        
+        await User.deleteMany();        
 
         console.log('Data Destroyed...'.red.inverse)
         process.exit();
