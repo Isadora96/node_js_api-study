7e840c89b20408c739465d161e7a0c70786233c3
isadora lopes <isadora.c.lopes@ibm.com>
2023-06-25 17:13:48 -0300

    Forgot password - Send Email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const ErrorResponse = require('../utils/errorResponse');
+const sendEmail = require('../utils/sendEmail');
 const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
 
@@ -69,6 +70,28 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
     await user.save({ validateBeforeSave: false });
 
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`;
+
+    const message = `You  are receiving this email because you has requested the reset of a password.
+    Please make a PUT request to: \n\n ${resetUrl}`;
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: 'Password reset token',
+            message
+        });
+
+        res.status(200).json({ success: true, data: 'Email sent' });
+    } catch (error) {
+        console.log(error);
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        await user.save({ validateBeforeSave: false });
+
+        return next(new ErrorResponse('Email could not be sent', 500));
+    }
     res.status(200).json({
         success: true,
         data: user
