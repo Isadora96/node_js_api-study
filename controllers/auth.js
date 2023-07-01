fa366b7c67d8eacb7ff03117fd9618705b87ddd2
isadora lopes <isadora.c.lopes@ibm.com>
2023-07-01 14:14:42 -0300

    Logout to clear token cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,7 +20,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     sendTokenResponse(user, 200, res);
 });
 
-// @desc    Register user
+// @desc    Login user
 // @route   POST /api/v1/auth/login
 // @access  Public
 exports.login = asyncHandler(async (req, res, next) => {
@@ -43,6 +43,21 @@ exports.login = asyncHandler(async (req, res, next) => {
     }
 
     sendTokenResponse(user, 200, res);
+});
+
+// @desc    Log user out / clear cookie
+// @route   GET /api/v1/auth/logout
+// @access  Private
+exports.logout = asyncHandler(async (req, res, next) => {
+    res.cookie('token', 'none', {
+        expires: new Date(Date.now() + 1),
+        httpOnly: true
+    });
+
+    res.status(200).json({
+        success: true,
+        data: {}
+    });
 });
 
 // @desc    Get current logged in user
